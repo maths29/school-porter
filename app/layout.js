f51abdf6c6838a0553ebd3b6c71ec55dcdaf7e5d
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-30 13:55:16 +0200

    Modify files

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <h2>header,navbar</h2>
+      {children}
+      <footer>footer</footer>
+      </body>
     </html>
   );
 }
