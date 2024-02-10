3fe623e79762498d0fd6d453ccd7b8309853722e
mathsoft29 <iwuconcept@gmail.com>
2024-02-10 13:50:20 +0100

    Add  Navbar components and include a search bar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,7 +4,7 @@ import { MdMenu } from "react-icons/md";
 const Navbar = () => {
   return (
     <nav className="bg-[#011431]">
-      <div>
+      <div className="flex gap-6 items-center">
         <span className="md:hidden flex justify-between text-white items-center px-1">
           <h1 className="text-xl font-bold">Swift School</h1>
           <MdMenu />
@@ -12,6 +12,60 @@ const Navbar = () => {
         <span className="hidden md:flex">
           <Image src="/logo.png" alt="Logo" width={80} height={80} />
         </span>
+        <ul className="flex gap-12">
+          <li className="text-orange-600">Home</li>
+          <li className="relative group">
+                <a href="/" className="text-white">Staff & HRD</a>
+                <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
+                  <li><a href="/" className="block px-4 py-2 text-white">Academic Staff</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Administrative Staff</a></li>
+                  
+                </ul>
+              </li>
+              <li className="relative group">
+                <a href="/" className="text-white">Academics</a>
+                <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
+                  <li><a href="/" className="block px-4 py-2 text-white">Admission policy</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Apply now</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Check Entrance Exam Result</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Reprint Application</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Take Entrance Exam</a></li>
+                </ul>
+              </li>
+              <li className="relative group">
+                <a href="/" className="text-white">Departments </a>
+                <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
+                  <li><a href="/" className="block px-4 py-2 text-white">Head Of Department</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Subject Offered</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Guidiance & Counselling</a></li>
+				  <li><a href="/" className="block px-4 py-2 text-white">Research & library</a></li>
+				  
+                </ul>
+              </li>
+              
+              <li className="relative group">
+                <a href="/" className="text-white">Academics</a>
+                <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
+                  <li><a href="/" className="block px-4 py-2 text-white">School Management Structure</a></li>
+                  <li><a href="/" className="block px-4 py-2 text-white">Academics</a></li>
+                  
+                </ul>
+              </li>
+          <li className="text-white">More Pages</li>
+          <li className="text-white">Accounts</li>
+          <li className="text-white">Library</li>
+        </ul>
+        <li>
+                <div className="relative">
+                  <input type="text" placeholder="Search" className="py-1 px-3 bg-gray-700 text-white rounded-md focus:outline-none focus:bg-gray-600" />
+                  <button className="absolute right-0 top-0 mt-1 mr-1 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M12.293 11.293a1 1 0 0 1-1.414 1.414l-2.5-2.5a1 1 0 1 1 1.414-1.414l2.5 2.5a1 1 0 0 1 0 1.414z" clipRule="evenodd" />
+                      <path fillRule="evenodd" d="M11 3a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
+              </li>
       </div>
     </nav>
   );
