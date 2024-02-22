f23647af9b919d9158add2dcc9ffa672dfa1eaaa
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-02-22 08:47:09 +0200

    Display background images

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Home from "./components/Home";
+import ImageCarousel from "./components/ImageCarousel";
 
 export default function home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Home />
+     <ImageCarousel />
     </main>
   );
 }
