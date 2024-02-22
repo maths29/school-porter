f373795b00aa341e5fa5e7042f80b7bbd98b29f0
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-02-22 10:20:24 +0200

    Update the component

--- app/components/ImageCarousel.js
+++ app/components/ImageCarousel.js
@@ -1,23 +1,63 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 const ImageCarousel = () => {
   const images = [
-    {url: '/schoolPorters1.jpg'},
-    {url: '/schoolPorters2.jpg'},
-    {url: '/schoolPorters3.jpg'},
-  ]
-  return (
-    <div  style={{
-      backgroundImage: `url(${images[0].url})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      width: '100%',
-      height: '48vh', // Adjust height as needed
-      border: '1px solid black', // Add border as needed
-    }} >
-      Images
-    </div>
-  )
-}
+    { url: "/schoolPorters1.jpg" },
+    { url: "/schoolPorters2.jpg" },
+    { url: "/schoolPorters3.jpg" },
+  ];
 
-export default ImageCarousel
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{
+          backgroundImage: `url(${images[currentIndex].url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="h-[60vh] w-full rounded-2xl"
+      >
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+        <div className="flex top-4 justify-center py-2">
+          {images.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="text-2xl text-red-600 cursor-pointer"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCarousel;
