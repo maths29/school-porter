fdd8582732c6cdb826db01bdee4c8f3cf385d78c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-02-22 08:48:02 +0200

    Update ImageCarousel component

--- app/components/ImageCarousel.js
+++ app/components/ImageCarousel.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const ImageCarousel = () => {
+  const images = [
+    {url: '/schoolPorters1.jpg'},
+    {url: '/schoolPorters2.jpg'},
+    {url: '/schoolPorters3.jpg'},
+  ]
   return (
-    <div>
-      
+    <div  style={{
+      backgroundImage: `url(${images[0].url})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      width: '100%',
+      height: '48vh', // Adjust height as needed
+      border: '1px solid black', // Add border as needed
+    }} >
+      Images
     </div>
   )
 }
