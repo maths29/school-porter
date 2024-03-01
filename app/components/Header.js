8c8305cfa92a97e501f10f34566edd41b27d3ed6
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-03-01 09:42:48 +0200

    Improve the header design

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,7 +3,7 @@ import { MdOutlineCall } from "react-icons/md";
 
 const Header = () => {
   return (
-    <header className="flex justify-end gap-4 p-2 items-center">
+    <header className="bg-[#ddd] flex justify-end gap-4 p-1 items-center">
       <div className="text-[#fd5f00] flex gap-1">
         <SiMinutemailer />
         <p className="text-black text-xs md:text-base">Email</p>
@@ -12,7 +12,7 @@ const Header = () => {
         <MdOutlineCall />
         <p className="text-black text-xs md:text-base">Call</p>
       </div>
-      <button className="bg-[#fd5f00] text-white px-3 py-2 border rounded-sm">Apply Now</button>
+      <button className="bg-[#fd5f00] text-white text-sm px-1 py-1 border rounded-xl">Apply Now</button>
     </header>
   );
 };
