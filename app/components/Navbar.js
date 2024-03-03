e67d9b12cca4eec6a63dff08b7743225648d3e83
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-03-03 11:43:07 +0200

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,467 +1,230 @@
-"use client";
-import { useState } from "react";
 import Image from "next/image";
-import { MdOutlineMenu, MdClose } from "react-icons/md";
+import { MdMenu } from "react-icons/md";
 import Link from "next/link";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState(false);
-
-  const handleOpen = () => {
-    setMenu(!menu);
-  };
-
-  const toggleSubMenu = (index) => {
-    const subMenu = document.getElementById(`submenu-${index}`);
-    const allSubMenus = document.querySelectorAll("[id^='submenu-']");
-    allSubMenus.forEach((menu) => {
-      if (menu !== subMenu) {
-        menu.classList.add("hidden");
-      }
-    });
-    subMenu.classList.toggle("hidden");
-  };
-
   return (
-    <nav className="bg-[rgb(1,20,49)] flex  p-2 md:px-8">
-      <div className="w-full flex flex-col gap-6 md:flex-row justify-between text-white md:items-center">
-        <div className="flex justify-between">
-          <Link href="/" className="flex gap-1 md:gap-2 items-center">
-            <Image src="/logo.png" alt="Logo" width={50} height={50} />
-            <h1 className="">Swift School</h1>
-          </Link>
-          {menu ? (
-            <button
-              className="md:hidden text-2xl cursor-pointer"
-              onClick={handleOpen}
-            >
-              <MdClose />
-            </button>
-          ) : (
-            <button
-              className="md:hidden text-2xl cursor-pointer"
-              onClick={handleOpen}
-            >
-              <MdOutlineMenu />
-            </button>
-          )}
-        </div>
-        <ul className="hidden md:flex justify-between gap-4 w-11/12 md:w-10/12">
-          <li className="hover:text-orange-600">
+    <nav className="bg-[#011431]">
+      <div className="flex gap-6 items-center">
+        <span className="md:hidden flex justify-between text-white items-center px-1">
+          <h1 className="text-xl font-bold">Swift School</h1>
+          <MdMenu />
+        </span>
+        <span className="hidden md:flex">
+          <Image src="/logo.png" alt="Logo" width={80} height={80} />
+        </span>
+        <ul className="flex gap-12">
+          <li className="text-orange-600">
             <Link href="/">Home</Link>
           </li>
           <li className="relative group">
-            <Link href="" className="">
+            <Link href="" className="text-white">
               Staff & HRD
             </Link>
             <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Academic Staff
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Administrative Staff
                 </Link>
               </li>
             </ul>
           </li>
           <li className="relative group">
-            <Link href="" className="">
+            <Link href="" className="text-white">
               Admission
             </Link>
             <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Admission policy
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Apply now
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Check Entrance Exam Result
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Reprint Application
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Take Entrance Exam
                 </Link>
               </li>
             </ul>
           </li>
           <li className="relative group">
-            <Link href="" className="">
+            <Link href="" className="text-white">
               Departments{" "}
             </Link>
             <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Head Of Department
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Subject Offered
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Guidiance & Counselling
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Research & library
                 </Link>
               </li>
             </ul>
           </li>
+
           <li className="relative group">
-            <Link href="" className="">
+            <Link href="" className="text-white">
               Academics
             </Link>
             <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   School Management Structure
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Academics
                 </Link>
               </li>
             </ul>
           </li>
           <li className="relative group">
-            <Link href="" className="">
+            <Link href="" className="text-white">
               More Pages
             </Link>
             <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Contact Us
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   News
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Calendar
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Tuition and fees
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Photo Gallery
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Definition Of Role
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   School Anthem & Pledge
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Horarium
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Honour Code
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Brief History
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Alumni Blogs
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Downloads
                 </Link>
               </li>
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Prospective Students info
                 </Link>
               </li>
             </ul>
           </li>
           <li className="relative group">
-            <Link href="" className="">
+            <Link href="" className="text-white">
               Accounts
             </Link>
             <ul className="absolute hidden group-hover:block bg-gray-800 rounded-lg mt-2">
               <li>
-                <Link href="" className="block ">
+                <Link href="" className="block px-4 py-2 text-white">
                   Login
                 </Link>
               </li>
             </ul>
           </li>
-          <li className="">
-            <Link href="">Library</Link>
-          </li>
-          {/* <li>
-              <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="py-1 px-3 bg-gray-700  rounded-md focus:outline-none focus:bg-gray-600"
-                />
-                <button className="absolute right-0 top-0 mt-1 mr-1 focus:outline-none">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 "
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.293 11.293a1 1 0 0 1-1.414 1.414l-2.5-2.5a1 1 0 1 1 1.414-1.414l2.5 2.5a1 1 0 0 1 0 1.414z"
-                      clipRule="evenodd"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      d="M11 3a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </li> */}
+          <li className="text-white">Library</li>
         </ul>
-        {menu && (
-          <ul className="md:hidden flex flex-col gap-2">
-            <li className="text-orange-600">
-              <Link href="/">Home</Link>
-            </li>
-            <li  onClick={() => toggleSubMenu(1)}>
-                Staff & HRD
-              <ul id="submenu-1">
-                <li>
-                  <Link href="" className="block ">
-                    Academic Staff
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Administrative Staff
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li onClick={() => toggleSubMenu(2)}>
-                Admission
-              <ul id="submenu-2">
-                <li>
-                  <Link href="" className="block ">
-                    Admission policy
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Apply now
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Check Entrance Exam Result
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Reprint Application
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Take Entrance Exam
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li  onClick={() => toggleSubMenu(3)}>
-                Departments
-              <ul id="submenu-3">
-                <li>
-                  <Link href="" className="block ">
-                    Head Of Department
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Subject Offered
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Guidiance & Counselling
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Research & library
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li  onClick={() => toggleSubMenu(4)}>
-                Academics
-              <ul id="submenu-4">
-                <li>
-                  <Link href="" className="block ">
-                    School Management Structure
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Academics
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li  onClick={() => toggleSubMenu(5)}>
-                More Pages
-              <ul id="submenu-5">
-                <li>
-                  <Link href="" className="block ">
-                    Contact Us
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    News
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Calendar
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Tuition and fees
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Photo Gallery
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Definition Of Role
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    School Anthem & Pledge
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Horarium
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Honour Code
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Brief History
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Alumni Blogs
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Downloads
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" className="block ">
-                    Prospective Students info
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li  onClick={() => toggleSubMenu(6)}>
-                Accounts
-              <ul id="submenu-6">
-                <li>
-                  <Link href="" className="block ">
-                    Login
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="">
-            <Link href="">Library</Link>
-          </li>
-            {/* <li>
-              <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="py-1 px-3 bg-gray-700  rounded-md focus:outline-none focus:bg-gray-600"
+        <li>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search"
+              className="py-1 px-3 bg-gray-700 text-white rounded-md focus:outline-none focus:bg-gray-600"
+            />
+            <button className="absolute right-0 top-0 mt-1 mr-1 focus:outline-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-white"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.293 11.293a1 1 0 0 1-1.414 1.414l-2.5-2.5a1 1 0 1 1 1.414-1.414l2.5 2.5a1 1 0 0 1 0 1.414z"
+                  clipRule="evenodd"
                 />
-                <button className="absolute right-0 top-0 mt-1 mr-1 focus:outline-none">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 "
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.293 11.293a1 1 0 0 1-1.414 1.414l-2.5-2.5a1 1 0 1 1 1.414-1.414l2.5 2.5a1 1 0 0 1 0 1.414z"
-                      clipRule="evenodd"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      d="M11 3a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </li> */}
-          </ul>
-        )}
+                <path
+                  fillRule="evenodd"
+                  d="M11 3a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </li>
       </div>
     </nav>
   );
